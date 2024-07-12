bb7bc40924b0d9ccf8fdd6ab9d339ef718ea4e96
Sonu Thakur <rsonuth789@gmail.com>
2024-07-12 18:45:01 +0545

    initial commit

---
+++ first/area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float radius;
+
+    printf("enter the radius\n");
+    scanf("%f", &radius);
+
+    printf("area is %.2f", 3.14*radius*radius);
+    return 0;
+}
